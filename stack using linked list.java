9d7ee8345614a0f8626909277007775805fec879
vishalyadav81295 <51483777+vishalyadav81295@users.noreply.github.com>
2019-06-09 12:40:41 +0530

    Create stack using linked list.java

---
+++ stack using linked list.java
@@ -0,0 +1,46 @@
+public class Main
+{
+	public static void main(String[] args) {
+		stack sk=new stack();
+	//	sk.push(10);
+	//	sk.push(13);
+	//	sk.push(3);
+	//	sk.printvalues();
+		System.out.println(sk.pop());
+		sk.printvalues();
+	}
+}
+class stack{
+    node top;
+    class node{
+        int data;
+        node next;
+        node(int a){
+            data=a;
+            next=null;
+        }
+        }
+         public void push(int a){
+            node newnode=new node(a);
+            newnode.next=top;
+            top=newnode;
+            
+        }
+        public void printvalues(){
+            node temp=top;
+            if(temp==null){System.out.println("it is empty.");}
+            else while(temp!=null){
+              System.out.print(temp.data+" "); 
+              temp=temp.next;
+            }
+        }
+        public int pop(){
+            if(top==null){System.out.println("Stack is underflow");
+                System.exit(0);
+            }
+            int a=top.data;
+            top=top.next; 
+            return a;
+            
+        }
+    }
