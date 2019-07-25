dffe4c359536fc1041f4d1b5f5788ee19adcce27
Vishal Kumar Yadav <51483777+vishalyadav81295@users.noreply.github.com>
2019-07-25 00:57:09 -0700

    Create to print diamond.java

---
+++ to print diamond.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter size of pyramid(odd number):");
+		int n=sc.nextInt();
+		int a=n/2;
+		for(int i=0;i<=a;i++){
+		for(int j=0;j<n;j++){
+		if(j>=a-i&&j<=a+i){
+		System.out.print(" * ");}
+		else System.out.print("   ");
+		}
+		System.out.println();}
+	    
+	    
+	
+		for(int i=a-1;i>=0;i--){
+		for(int j=0;j<n;j++){
+		if(j>=a-i&&j<=a+i){
+		System.out.print(" * ");}
+		else System.out.print("   ");
+		}
+		System.out.println();}
+	}
+}
